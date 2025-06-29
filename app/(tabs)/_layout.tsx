7e6863bff7a502ec37ad08d201af44979cedd116
roth <68660484+roth-dev@users.noreply.github.com>
2025-06-29 17:09:28 +0700

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Search, PlusSquare, Heart, User } from 'lucide-react-native';
+import { Chrome as Home, Search, SquarePlus as PlusSquare, Heart, User } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (

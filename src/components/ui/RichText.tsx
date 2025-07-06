f191b8d090a6d76c00db9b55d25bbde119b6e199
roth-dev <roth.dev.ops@gmail.com>
2025-07-06 15:47:33 +0700

    feat: handle user name press richtext

--- src/components/ui/RichText.tsx
+++ src/components/ui/RichText.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { memo, useMemo } from "react";
 import { Linking, TextStyle } from "react-native";
 import { AppBskyRichtextFacet, RichText as RichTextAPI } from "@atproto/api";
 import { Text, TextProps } from "./Text";
@@ -18,113 +18,117 @@ export interface RichTextProps extends TextProps {
   hashtagClassName?: string;
 }
 
-export function RichText({
-  value,
-  disableLinks = false,
-  enableTags = false,
-  onLinkPress,
-  interactiveStyle,
-  emojiMultiplier = 1.85,
-  numberOfLines,
-  linkClassName,
-  mentionClassName,
-  hashtagClassName,
-  ...props
-}: RichTextProps) {
-  const richText = useMemo(
-    () =>
-      value instanceof RichTextAPI ? value : new RichTextAPI({ text: value }),
-    [value]
-  );
+const RichText = memo(
+  ({
+    value,
+    disableLinks = false,
+    enableTags = false,
+    onLinkPress,
+    interactiveStyle,
+    emojiMultiplier = 1.85,
+    numberOfLines,
+    linkClassName,
+    mentionClassName,
+    hashtagClassName,
+    ...props
+  }: RichTextProps) => {
+    const richText = useMemo(
+      () =>
+        value instanceof RichTextAPI ? value : new RichTextAPI({ text: value }),
+      [value]
+    );
 
-  const { text, facets } = richText;
+    const { text, facets } = richText;
 
-  // If no special facets (links, mentions, hashtags), return plain text
-  if (!facets?.length) {
+    // If no special facets (links, mentions, hashtags), return plain text
+    if (!facets?.length) {
+      return (
+        <Text {...props} numberOfLines={numberOfLines}>
+          {text}
+        </Text>
+      );
+    }
+
+    const elements: React.ReactNode[] = [];
+    let key = 0;
+
+    for (const segment of richText.segments()) {
+      const { text } = segment;
+      const link = segment.link;
+      const mention = segment.mention;
+      const tag = segment.tag;
+
+      if (
+        mention &&
+        AppBskyRichtextFacet.validateMention(mention).success &&
+        !disableLinks
+      ) {
+        elements.push(
+          <Text
+            key={key}
+            style={[interactiveStyle]}
+            font="semiBold"
+            suppressHighlighting
+            {...props}
+            className={cn("text-blue-500 underline", mentionClassName)}
+            onPress={() => {
+              router.push(`/profile/${text.slice(1)}`);
+            }}
+          >
+            {text}
+          </Text>
+        );
+      } else if (link && AppBskyRichtextFacet.validateLink(link).success) {
+        elements.push(
+          <Text
+            key={key}
+            numberOfLines={1}
+            style={interactiveStyle}
+            suppressHighlighting
+            {...props}
+            className={cn("text-blue-500 underline", linkClassName)}
+            onPress={() =>
+              onLinkPress ? onLinkPress(link.uri) : Linking.openURL(link.uri)
+            }
+          >
+            {text}
+          </Text>
+        );
+      } else if (
+        !disableLinks &&
+        enableTags &&
+        tag &&
+        AppBskyRichtextFacet.validateTag(tag).success
+      ) {
+        elements.push(
+          <Text
+            key={key}
+            style={interactiveStyle}
+            suppressHighlighting
+            {...props}
+            className={cn("text-blue-500 underline", hashtagClassName)}
+            onPress={() => {
+              // router.push({
+              //   pathname: "/(app)/",
+              //   params: { tag: tag.tag },
+              // });
+            }}
+          >
+            {text}
+          </Text>
+        );
+      } else {
+        elements.push(text);
+      }
+      key++;
+    }
+
     return (
       <Text {...props} numberOfLines={numberOfLines}>
-        {text}
+        {elements}
       </Text>
     );
   }
+);
 
-  const elements: React.ReactNode[] = [];
-  let key = 0;
-
-  for (const segment of richText.segments()) {
-    const { text } = segment;
-    const link = segment.link;
-    const mention = segment.mention;
-    const tag = segment.tag;
-
-    if (
-      mention &&
-      AppBskyRichtextFacet.validateMention(mention).success &&
-      !disableLinks
-    ) {
-      elements.push(
-        <Text
-          key={key}
-          style={[interactiveStyle]}
-          font="semiBold"
-          suppressHighlighting
-          {...props}
-          className={cn("text-blue-500 underline", mentionClassName)}
-          onPress={() => {
-            // router.push(`/user-profile/${mention.did}`);
-          }}
-        >
-          {text}
-        </Text>
-      );
-    } else if (link && AppBskyRichtextFacet.validateLink(link).success) {
-      elements.push(
-        <Text
-          key={key}
-          numberOfLines={1}
-          style={interactiveStyle}
-          suppressHighlighting
-          {...props}
-          className={cn("text-blue-500 underline", linkClassName)}
-          onPress={() =>
-            onLinkPress ? onLinkPress(link.uri) : Linking.openURL(link.uri)
-          }
-        >
-          {text}
-        </Text>
-      );
-    } else if (
-      !disableLinks &&
-      enableTags &&
-      tag &&
-      AppBskyRichtextFacet.validateTag(tag).success
-    ) {
-      elements.push(
-        <Text
-          key={key}
-          style={interactiveStyle}
-          suppressHighlighting
-          {...props}
-          className={cn("text-purple-500 underline", hashtagClassName)}
-          onPress={() => {
-            // router.push({
-            //   pathname: "/(app)/",
-            //   params: { tag: tag.tag },
-            // });
-          }}
-        >
-          {text}
-        </Text>
-      );
-    } else {
-      elements.push(text);
-    }
-    key++;
-  }
-
-  return (
-    <Text {...props} numberOfLines={numberOfLines}>
-      {elements}
-    </Text>
-  );
-}
+export { RichText };

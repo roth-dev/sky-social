aa14c269fa5534af39cfb8a05d90bdc5f0f500fc
roth-dev <roth.dev.ops@gmail.com>
2025-07-12 23:27:07 +0700

    feat: refactor PagerView to use forwardRef

--- src/components/pager/index.tsx
+++ src/components/pager/index.tsx
@@ -1,2 +1,10 @@
-import PagerView from "react-native-pager-view";
+import { forwardRef } from "react";
+import Pager, { PagerViewProps } from "react-native-pager-view";
+
+const PagerView = forwardRef<Pager, PagerViewProps>((props, ref) => (
+  <Pager ref={ref} {...props} />
+));
+
+PagerView.displayName = "PagerView";
+
 export default PagerView;

279f67a9d820f02d298e09da0f344db62ec18ae2
roth-dev <roth.dev.ops@gmail.com>
2025-08-15 09:54:35 +0700

    feat: refactor Loading component to simplify size handling and remove unused variants

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,29 +1,13 @@
-import { cn } from "@/lib/utils";
-import { cva, VariantProps } from "class-variance-authority";
 import { memo } from "react";
-import { ActivityIndicator } from "react-native";
-const loadingVariants = cva(
-  "flex-1 py-2", // base
-  {
-    variants: {
-      size: {
-        xs: "w-3 h-3",
-        md: "w-5 h-5",
-        lg: "w-7 h-7",
-        "2xl": "w-10 h-10",
-        "3xl": "w-14 h-14",
-      },
-    },
-    defaultVariants: {
-      size: "md",
-    },
-  }
-);
-
+import { ActivityIndicator, StyleProp, ViewStyle } from "react-native";
 const Loading = memo(function Comp({
-  size,
-}: VariantProps<typeof loadingVariants>) {
-  return <ActivityIndicator className={cn(loadingVariants({ size }))} />;
+  size = "small",
+  style,
+}: {
+  size: "small" | "large";
+  style?: StyleProp<ViewStyle>;
+}) {
+  return <ActivityIndicator size={size} style={style} />;
 });
 
 export default Loading;

2a7557bd6ee9aa4b6bf0007d67a5a8edc46521b6
roth-dev <roth.dev.ops@gmail.com>
2025-08-17 16:10:01 +0700

    feat: refactor LightBox store to manage images and state more effectively

--- src/store/lightBox.ts
+++ src/store/lightBox.ts
@@ -1,11 +1,26 @@
+import { EmbedImage } from "@/types/embed";
 import { create } from "zustand";
 
 type LightBoxStore = {
-  isOpen: boolean;
-  setValue: (open: boolean) => void;
+  index: number | undefined;
+  shareId: string | undefined;
+  images: EmbedImage[];
+  setValue: (
+    images: EmbedImage[],
+    index: number | undefined,
+    shareId: string | undefined
+  ) => void;
+  clearValue: () => void;
 };
 
 export const useLightBoxOpen = create<LightBoxStore>((set) => ({
-  isOpen: false,
-  setValue: (isOpen: boolean) => set({ isOpen }),
+  index: 0,
+  shareId: "",
+  images: [],
+  setValue: (
+    images: EmbedImage[],
+    index: number | undefined,
+    shareId: string | undefined
+  ) => set({ images, index, shareId }),
+  clearValue: () => set({ images: [], index: undefined, shareId: undefined }),
 }));
